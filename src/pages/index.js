95d3b25584bceb40f0fce5f653b350afcb21374f
Clovis Rosa <35171486+clovis-rosa@users.noreply.github.com>
2020-06-04 15:06:51 +0100

    Added video and footers sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,25 @@ import React from "react"
 // import { Link } from "gatsby"
 import "../styles/styles.scss"
 
+// Scrollbar
+// import Scrollbar from "smooth-scrollbar"
+
 // import Layout from "../components/layout"
 // import Image from "../components/image"
 // import SEO from "../components/seo"
 import Header from "../components/header"
 import Banner from "../components/Banner"
 import AboutBlurb from "../components/AboutBlurb"
+import VideoSection from "../components/videoSection"
+import Footer from "../components/footer"
 
 const IndexPage = () => (
   <>
     <Header />
     <Banner />
     <AboutBlurb />
+    <VideoSection />
+    <Footer />
   </>
 )
 
